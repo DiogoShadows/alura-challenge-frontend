b407196e3a4a0372707db05cde4011729c85e7ec
Diogo Santos <diogosantos@cobrape.com.br>
2022-06-08 12:51:29 -0300

    - Bulma add - Logo no topo esquerdo da tela

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="columns is-desktop">
+    <div class="column p-5">
+      <img alt="Logo alura" src="@/assets/logo.png" id="logo"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<style scoped>
+    #logo{
+        width: 200px;
+    }
+</style>
